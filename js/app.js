958a023b1031f6e2a2479baa33ce38e59e987d0b
eduardocd18 <javierecd@gmail.com>
2026-02-18 04:59:44 -0400

    Corrección

--- js/app.js
+++ js/app.js
@@ -5,42 +5,35 @@ botones.forEach((boton) => {
   boton.addEventListener("click", () => {
     const botonApretado = boton.textContent;
 
-    /* -- Borrar la pantalal al precionar "C" */
-    if (boton.id === "c" || boton.id === "C") {
+    if (boton.id === "c") {
       pantalla.textContent = "0";
-      return; // Al precionar "c", termina la función
+      return;
     }
 
-    /* -- Borrar último dígito -- */
     if (boton.id === "borrar") {
       if (
         pantalla.textContent.length === 1 ||
-        pantalla.textContent === "Error"
+        pantalla.textContent === "Error!"
       ) {
         pantalla.textContent = "0";
       } else {
         pantalla.textContent = pantalla.textContent.slice(0, -1);
       }
-
       return;
-      return; // Al precionar borrar, termina la función
     }
 
-    /* -- Realizar la operación al presionar el botón de igual */
     if (boton.id === "igual") {
       try {
-        pantalla.textContent = eval(pantalla.textContent); // Hace la operación matemática que tenga en pantalla.
-      } catch (error) {}
-      pantalla.textContent = "Error";
+        pantalla.textContent = eval(pantalla.textContent);
+      } catch {
+        pantalla.textContent = "Error!";
+      }
       return;
     }
 
-    /* -- Validar si en la pantalla está en 0 -- */
-    if (pantalla.textContent === "0" || pantalla.textContent === "Error") {
-      // Esto reemplaza el 0 inicial por el contenido del botón presionado
+    if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
       pantalla.textContent = botonApretado;
     } else {
-      // Suma los valores que se vayan agregando al ir presinando botones
       pantalla.textContent += botonApretado;
     }
   });
